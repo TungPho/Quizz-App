e5eea38365f2469cf85a97fab4e9410d1f8a79f2
Tung Pho <ductungpho1005@gmail.com>
2025-02-13 15:18:09 +0700

    update 4

--- backend/src/routers/main.route.js
+++ backend/src/routers/main.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const userRoute = require("./routes/user.route");
 const questionRoute = require("./routes/question.route");
 const testRoute = require("./routes/test.route");
+const classRoute = require("./routes/class.route");
 
 const mainRouter = express.Router();
 mainRouter.use("/api/v1", userRoute);
 mainRouter.use("/api/v1/", questionRoute);
 mainRouter.use("/api/v1/", testRoute);
+mainRouter.use("/api/v1/", classRoute);
 
 module.exports = mainRouter;
